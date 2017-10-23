4fc85f4bdfae5d81047c8b0a0ebb387b178c77e8
Julianne Kim <juliannne@gmail.com>
2017-10-23 18:37:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,6 +29,9 @@ class Transfer
       sender.balance = sender.balance + amount
       receiver.balance = receiver.balance - amount
       self.status = "reversed"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
 end
